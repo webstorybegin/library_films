b2d9eb6efbb59c225b515461ce05d9604ff525c7
Dmitrii Danilov <fd.dmitriidanilov@gmail.com>
2024-02-06 11:12:08 +0200

    created electron app

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.setMenuBarVisibility(false);
+  // Development
+  mainWindow.loadURL("http://localhost:3000/films_library");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
